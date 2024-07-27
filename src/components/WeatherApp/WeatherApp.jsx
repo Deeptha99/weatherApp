61bb029dd1bb38f337cf75589bd8a15312d013b2
Deeptha Ranaweera <deeptharanaweera26@gmail.com>
2024-07-27 15:52:06 +0530

    update cloud png

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './WeatherApp.css';
 
+import clear_icon from "../Assets/clear.png";
 import cloud_icon from "../Assets/cloud.png";
 import humidity_icon from "../Assets/humidity.png";
+import rain_icon from "../Assets/rain.png";
 import search_icon from "../Assets/search.png";
 import wind_icon from "../Assets/wind.png";
 
 export const WeatherApp = () => {
+  const [weatherIcon, setWeatherIcon] = useState(cloud_icon);
+  const api_key = "ed18ce5a059ac5efff520442da84f47d";
 
-  let api_key = "ed18ce5a059ac5efff520442da84f47d";
+  const getWeatherIcon = (condition) => {
+    if (condition.includes("cloud")) {
+      return cloud_icon;
+    } else if (condition.includes("clear")) {
+      return clear_icon;
+    } else if (condition.includes("rain")) {
+      return rain_icon;
+    } else {
+      return cloud_icon;
+    }
+  }
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
@@ -33,6 +47,9 @@ export const WeatherApp = () => {
         wind[0].innerHTML = data.wind.speed + ' km/h';
         temperature[0].innerHTML = data.main.temp + '°c';
         location[0].innerHTML = data.name;
+
+        const condition = data.weather[0].description.toLowerCase();
+        setWeatherIcon(getWeatherIcon(condition));
       } else {
         toast.error("City not found. Please enter a valid city name.");
       }
@@ -43,43 +60,45 @@ export const WeatherApp = () => {
   }
 
   return (
-    <div className='container'>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      <div className='top-bar'>
-        <input type="text" name="cityInput" className="cityInput" placeholder='Search here.....' id="" />
-        <div className='search-icon' onClick={search}>
-          <img src={search_icon} alt="search" />
-        </div>
-      </div>
-      <div className='weather-image'>
-        <img src={cloud_icon} alt="cloud" />
-      </div>
-      <div className='weather-temp'>24°c</div>
-      <div className='weather-location'>Sri Lanka</div>
-      <div className='data-container'>
-        <div className='element'>
-          <img src={humidity_icon} alt="humidity" className='icon' />
-          <div className='data'>
-            <div className='humidity-percent'>64%</div>
-            <div className='text'>Humidity</div>
+    <div className='main'>
+      <div className='container'>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        <div className='top-bar'>
+          <input type="text" name="cityInput" className="cityInput" placeholder='Search here.....' id="" />
+          <div className='search-icon' onClick={search}>
+            <img src={search_icon} alt="search" />
           </div>
         </div>
-        <div className='element'>
-          <img src={wind_icon} alt="wind" className='icon' />
-          <div className='data'>
-            <div className='wind-rate'>18 km/h</div>
-            <div className='text'>Wind Speed</div>
+        <div className='weather-image'>
+          <img src={weatherIcon} alt="weather icon" />
+        </div>
+        <div className='weather-temp'>24°c</div>
+        <div className='weather-location'>Sri Lanka</div>
+        <div className='data-container'>
+          <div className='element'>
+            <img src={humidity_icon} alt="humidity" className='icon' />
+            <div className='data'>
+              <div className='humidity-percent'>64%</div>
+              <div className='text'>Humidity</div>
+            </div>
+          </div>
+          <div className='element'>
+            <img src={wind_icon} alt="wind" className='icon' />
+            <div className='data'>
+              <div className='wind-rate'>18 km/h</div>
+              <div className='text'>Wind Speed</div>
+            </div>
           </div>
         </div>
       </div>
